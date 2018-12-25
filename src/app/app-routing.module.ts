36a5a39cffd7b735b392b75a4f6b25356ae68f83
Lillian Lee <lillianlee62@gmail.com>
2018-12-25 10:29:26 -0500

    add ng7

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { Routes, RouterModule} from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
